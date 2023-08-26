76fc7b3dd7bfa7628b5ce888c9070772a8c40e29
lzoxun-redmi <869664233@qq.com>
2023-08-26 21:38:58 +0800

    2023-08-26 21:38提交上传

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -2,14 +2,17 @@
 	<div class="dashboard-page page">
 		<div class="head-count">
 			<el-row :gutter="10">
-				<el-col :span="6"><div class="grid-content ep-bg-purple">
-					<my-progress></my-progress>
-				</div> </el-col>
-				<el-col :span="6"><div class="grid-content ep-bg-purple">统计</div> </el-col>
+				<el-col :span="6"
+					><div class="grid-content ep-bg-purple">
+						<!-- <my-progress></my-progress> -->
+					</div>
+				</el-col>
+				<el-col :span="6"><div class="grid-content ep-bg-purple" @click="changeline">统计</div> </el-col>
 				<el-col :span="6"><div class="grid-content ep-bg-purple">统计</div> </el-col>
 				<el-col :span="6"><div class="grid-content ep-bg-purple">统计</div> </el-col>
 			</el-row>
 		</div>
+		<test :data3="listData3" :count="count"></test>
 		<div class="main-echarts">
 			<el-row :gutter="10">
 				<el-col :md="8" :sm="12" :xs="24">
@@ -50,7 +53,41 @@
 
 <script setup lang="ts">
 	import { PieEchart, LineEchart, MapEchart, KLineEchart, BarStackEchart } from '@/components/PageEcharts'
-	import MyProgress from './dash-cpns/my-progress.vue';
+	import MyProgress from './dash-cpns/my-progress.vue'
+	import test from './dash-cpns/test.vue'
+	import { ref } from 'vue'
+
+	let count = ref(5)
+	let listData3 = ref([
+		{
+			full_name: '张秀妹5',
+			phone_number: '',
+			job_name: '组长',
+			organization_name: '',
+			job_type: 1
+		}
+	])
+
+	//换线
+	const changeline = () => {
+		let ddd = [
+			{
+				full_name: '1张秀妹1',
+				phone_number: '',
+				job_name: '组长',
+				organization_name: '',
+				job_type: 1
+			}
+		]
+		// setTimeout(() => {
+		listData3.value = ddd
+		count.value = 10
+		console.log(listData3)
+		// }, 1000)
+
+		//打开选择批次窗体
+		//  refworkshop.value.showDialog()
+	}
 </script>
 
 <style lang="scss" scoped>
